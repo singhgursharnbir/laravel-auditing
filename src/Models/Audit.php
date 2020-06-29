e26c1e3cb60eb1653087898720081f486a4d030a
singhgursharnbir <61758414+singhgursharnbir@users.noreply.github.com>
2020-06-29 10:31:04 -0600

    Added code to make auditing work with laravel scout

--- src/Models/Audit.php
+++ src/Models/Audit.php
@@ -3,9 +3,22 @@
 namespace OwenIt\Auditing\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Audit extends Model implements \OwenIt\Auditing\Contracts\Audit
 {
+      use Searchable;
+
+    /**
+     * Get the index name for the model.
+     *
+     * @return string
+     */
+    public function searchableAs()
+    {
+        return 'audits';
+    }
+    
     use \OwenIt\Auditing\Audit;
 
     /**
@@ -21,4 +34,13 @@ class Audit extends Model implements \OwenIt\Auditing\Contracts\Audit
         'new_values'   => 'json',
         // Note: Please do not add 'auditable_id' in here, as it will break non-integer PK models
     ];
+    
+    
+    protected static function boot()
+    {
+        parent::boot();
+        static::created(function ($model) {
+            $model->searchable();
+        });
+    }
 }
